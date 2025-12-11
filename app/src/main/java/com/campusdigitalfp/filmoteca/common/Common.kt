70af335c3cf170d9be813b498fe7200ad5d428ba
tecnosue <susanaparacuellos@gmail.com>
2025-12-11 21:37:21 +0100

    Ejercicio 19. Botón HOME

--- app/src/main/java/com/campusdigitalfp/filmoteca/common/Common.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/common/Common.kt
@@ -3,8 +3,13 @@ package com.campusdigitalfp.filmoteca.common
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -20,6 +25,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.campusdigitalfp.filmoteca.R
 import com.campusdigitalfp.filmoteca.R.drawable.default_movie
@@ -50,14 +58,35 @@ fun BarraSuperiorComun(
         ) },
         navigationIcon = {
             if (mostrarAtras) {
-                IconButton(onClick = onAtrasClick) {
+                Box(
+                    modifier = Modifier
+                        .clickable {
+                            navController.popBackStack(
+                                "filmList",
+                                inclusive = false
+                            )
+                        }
+                        .padding(16.dp)
+                ) {
                     Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Atrás"
+                        imageVector = Icons.Default.Home,
+                        contentDescription = stringResource(R.string.volver_a_inicio),
+                        modifier = Modifier.size(24.dp)
                     )
                 }
             }
+
         },
+//        navigationIcon = {
+//            if (mostrarAtras) {
+//                IconButton(onClick = onAtrasClick) {
+//                    Icon(
+//                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+//                        contentDescription = "Atrás"
+//                    )
+//                }
+//            }
+//        }
         //menú en FilmListScreen
         actions = {
         if (mostrarMenu ){
